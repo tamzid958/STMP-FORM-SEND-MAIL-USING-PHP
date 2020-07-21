9afe96d891f053434d52a85d01fb8275b0283c9c
Tamzid Ahmed <tamjidahmed958@gmail.com>
2020-07-21 20:15:04 +0600

    code uploaded

---
+++ js/app.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+ $("input[name='donation']").change(function(){
+    if($(this).val()=="other")
+    {
+      $("#otherDonation").show();
+      TweenMax.from("#otherDonation", 1, {y:-10,scale:0.85});
+      TweenMax.to("#otherDonation", 1, { opacity:1 , delay:2 });
+    }
+    else
+    {
+    $("#otherDonation").hide();
+    }
+  });
+
+ 
+
+
+	$("input[name='regulardonate']").change(function(){
+    if ($(this).is(':checked')) 
+    {
+      $('div.div1').show();
+    TweenMax.from(".div1", 1, {y:-10,scale:0.85});
+    TweenMax.to(".div1", 1, { opacity:1 , delay:2 });
+    }
+		else $('div.div1').hide();
+    }).change();   
+  }
+
+  
+
+);
+/*
+function validateStep1()
+{
+  var fname =document.forms["step1"]["fname"];
+  var lname =document.forms["step1"]["lname"];
+  var company=document.forms["step1"]["company"];
+  var address1=document.forms["step1"]["address1"];
+  var city =document.forms["step1"]["city"];
+  var state =document.forms["step1"]["state"];
+  var zip =document.forms["step1"]["zip"];
+  var country =document.forms["step1"]["country"];
+  var email =document.forms["step1"]["email"];
+  var donation =document.forms["step1"]["donation"];
+  var otherDonation = document.forms["step1"]["otherDonation"];
+  if (fname.value == "")                                  
+  { 
+      window.alert("Please enter your first name."); 
+      fname.focus();
+      return false; 
+  } 
+
+  if (lname.value == "")                                  
+  { 
+      window.alert("Please enter your Last name."); 
+      lname.focus(); 
+      return false; 
+  } 
+  if (company.value == "")                                  
+  { 
+      window.alert("Please enter your company name."); 
+      company.focus(); 
+      return false; 
+  } 
+
+  if (address1.value == "")                                  
+  { 
+      window.alert("Please enter your Address."); 
+      address1.focus(); 
+      return false; 
+  } 
+
+  if (city.value == "")                                  
+  { 
+      window.alert("Please enter your City."); 
+      city.focus(); 
+      return false; 
+  } 
+
+  if (state.value == "")                                  
+  { 
+      window.alert("Please enter your state."); 
+      state.focus(); 
+      return false; 
+  } 
+
+  if (zip.value == "")                                  
+  { 
+      window.alert("Please enter your zip."); 
+      zip.focus(); 
+      return false; 
+  } 
+
+  if (country.value == "")                                  
+  { 
+      window.alert("Please enter your country."); 
+      country.focus(); 
+      return false; 
+  } 
+
+  if (email.value == "")                                  
+  { 
+      window.alert("Please enter your Email."); 
+      email.focus(); 
+      return false; 
+  } 
+
+  if (city.value == "")                                  
+  { 
+      window.alert("Please enter your City."); 
+      city.focus(); 
+      return false; 
+  } 
+  
+    if(donation.value =="other")
+    {
+      if(otherDonation.value == "")
+      {
+        window.alert("Please enter your Donation Amount."); 
+        otherDonation.focus(); 
+        return false;    
+      }
+   
+    }               
+  
+  return true;  
+}*/
---
+++ php/mailer.php
@@ -0,0 +1,112 @@
+<?php
+
+if(isset($_POST['confirmall']))
+{
+
+    
+    $fname=$_POST['fname'];
+
+    $lname=$_POST['lname'];
+
+    $company=$_POST['company'];
+    
+    $address1=$_POST['address1'];
+    
+    $address2=$_POST['address2'];
+    
+    $city=$_POST['city'];
+
+    $state=$_POST['state'];
+
+    $zip=$_POST['zip'];
+    
+    $country=$_POST['country'];
+
+    $tel=$_POST['tel'];
+    
+    $fax=$_POST['fax'];
+    
+    $email=$_POST['email'];
+    
+    $donation=$_POST['donation'];
+
+    $otherDonation=$_POST['otherDonation'];
+    
+    $regulardonate=$_POST['regulardonate'];
+
+    $dollar=$_POST['dollar'];
+
+    $months=$_POST['months'];
+
+
+    $Honorarirum=$_POST['Honorarirum'];
+    $name=$_POST['name'];
+    $acknowledge=$_POST['acknowledge'];
+    $address3=$_POST['address3'];
+    $city2=$_POST['city2'];
+    $state2= $_POST['state2'];
+    $zip2=$_POST['zip2'];
+
+    $coname= $_POST['coname'];
+    $anonymous= $_POST['anonymous'];
+    $mail_gift=$_POST['mail_gift'];
+    $not_mail=$_POST['not_mail'];
+    $comment=$_POST['comment'];
+    $contact_mail=$_POST['contact_mail'];
+    $contact_pmail=$_POST['contact_pmail'];
+    $contact_tel= $_POST['contact_tel'];
+    $contact_fax=$_POST['contact_fax'];
+    $newsletter_mail=$_POST['newsletter_mail'];
+    $newsletter_pmail=$_POST['newsletter_pmail'];
+    $volunteer=$_POST['volunteer'];
+
+
+
+
+  $to = "tamjidahmed958@gmail.com";
+  $cc = $email;
+  $email_subject ="Donation Form";
+
+  $email_body="
+               First Name: $fname 
+               Last Name:  $lname
+               Company Name: $company 
+               Address1: $address1  
+               Address2: $address2 
+               City: $city 
+               State: $state 
+               Zip: $zip 
+               Country: $country
+               Phone Number: $tel
+               Fax Number: $fax
+               Email Address: $email
+               Donation Amount: $donation / $otherDonation
+               Regular Donation: $regulardonate 
+               Regular Donation Amount: $dollar 
+               Month: $months 
+               I would like to make this dontation: $Honorarirum 
+               Honor name : $name 
+               Acknowledge Donation to : $acknowledge 
+               Honor's Address : $address3 
+               Honor's City = $city2 
+               Honor's Sate = $state2 
+               Honor's Zip code = $zip2  
+               Name, Company or Organization to appear in our publicatipns: $coname 
+               I would like my gift to remain anonymous: $anonymous 
+               My Employer offers a matching gift program.I will mail the matching gift form: $mail_gift 
+               Please save the cost acknowledge this gift by not mailing a thank you letter: $not_mail 
+               Comment: $comment 
+               How may we contact you?: $contact_mail , $contact_pmail, $contact_tel, $contact_fax 
+               I would like to receive newsletters and Information about special events by: $newsletter_mail, $newsletter_pmail 
+               I would like to get the information about volunteering with the email: $volunteer
+               ";
+
+  $email_body = wordwrap($email_body,1000);
+
+  mail($to, $email_subject,$email_body);
+  mail($cc, $email_subject,$email_body);
+  header ("Location: ../thanks.html");
+
+}
+
+  ?>
